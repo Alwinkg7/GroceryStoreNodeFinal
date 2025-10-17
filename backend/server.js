46215c2989a8e80fcd82368997b020812ae6d38b
Alwin K G <alwinkgofficial@gmail.com>
2025-10-17 12:18:09 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendPath));
 
   // Use a function for catch-all route
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
