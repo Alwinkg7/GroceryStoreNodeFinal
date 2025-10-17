a261c4ec16e03a271531387698094c8310e6e81b
Alwin K G <alwinkgofficial@gmail.com>
2025-10-17 11:17:06 +0530

    update to deploy

--- backend/server.js
+++ backend/server.js
@@ -38,3 +38,14 @@ app.use("/api/posts", commentRoutes);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+
+import path from "path";
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+}
