b2090f18a8b8f61bc9b3ac74819f2d4c8cfe33a4
Alwin K G <alwinkgofficial@gmail.com>
2025-10-17 12:07:34 +0530

    update on deployment

--- backend/server.js
+++ backend/server.js
@@ -44,8 +44,11 @@ import path from "path";
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("/", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
+  // Serve static frontend files
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // use 'build' if CRA
+
+  // Catch all routes and send index.html
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
 }
